68667ae400d6542c68ea383d992601c943aedc32
RamiroRomera <112445180+RamiroRomera25@users.noreply.github.com>
2024-11-15 12:41:31 -0300

    Method for SoftDelete and Reactivate

--- src/main/java/ar/edu/utn/frc/tup/lciii/templateSpring/repositories/GenericRepository.java
+++ src/main/java/ar/edu/utn/frc/tup/lciii/templateSpring/repositories/GenericRepository.java
@@ -1,16 +1,44 @@
 package ar.edu.utn.frc.tup.lciii.templateSpring.repositories;
 
+import ar.edu.utn.frc.tup.lciii.templateSpring.entities.base.BaseEntity;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.NoRepositoryBean;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.Optional;
 
 @NoRepositoryBean
 public interface GenericRepository<E, ID> extends JpaRepository<E, ID>, JpaSpecificationExecutor<E> {
     Page<E> findAll(Specification<E> filter, Pageable pageable);
     List<E> findAll(Specification<E> filter);
+
+
+    // Si E extiende de Base Entity se pueden usar estos metodos
+//    default E softDeleteById(ID id) {
+//        Optional<E> optionalE = this.findById(id);
+//        if (optionalE.isPresent()) {
+//            optionalE.get().setIsActive(false);
+//            this.save(optionalE.get());
+//        } else {
+//            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Not found any entity with id " + id);
+//        }
+//        return null;
+//    }
+
+//    default E reactivateById(ID id) {
+//        Optional<E> optionalE = this.findById(id);
+//        if (optionalE.isPresent()) {
+//            optionalE.get().setIsActive(true);
+//            this.save(optionalE.get());
+//        } else {
+//            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Not found any entity with id " + id);
+//        }
+//        return null;
+//    }
 }
