b028f1866d99cb378d7287cf2af24128103d3fe2
RamiroRomera <112445180+RamiroRomera25@users.noreply.github.com>
2024-11-15 11:21:47 -0300

    Better Abstract Filters

--- src/main/java/ar/edu/utn/frc/tup/lciii/templateSpring/repositories/specs/GenericSpecification.java
+++ src/main/java/ar/edu/utn/frc/tup/lciii/templateSpring/repositories/specs/GenericSpecification.java
@@ -17,39 +17,20 @@ import java.util.Map;
 @NoArgsConstructor
 @Component
 public class GenericSpecification<E extends BaseEntity> {
-
-    /**
-     * Filter by date.
-     * @param startDate start
-     * @param endDate end
-     * @return Specification with filtered dates.
-     */
-    public Specification<E> filterByDateRange(LocalDateTime startDate, LocalDateTime endDate) {
-        return (root, query, criteriaBuilder) -> {
-            if (startDate != null && endDate != null) {
-                return criteriaBuilder.between(root.get("createdDate"), startDate, endDate);
-            } else if (startDate != null) {
-                return criteriaBuilder.greaterThanOrEqualTo(root.get("createdDate"), startDate);
-            } else if (endDate != null) {
-                return criteriaBuilder.lessThanOrEqualTo(root.get("createdDate"), endDate);
-            }
-            return criteriaBuilder.conjunction();
-        };
-    }
-
+    
     /**
      * Filter by attributtes.
-     * @param attributtes att for dinamyc search
+     * @param attributes att for dinamyc search
      *  On key value u will put the name of the att and
      *  in the value the "value" that u want to search on
      *  that att.
      * @return Specification att.
      */
-    public Specification<E> dynamicFilter(Map<String, Object> attributtes) {
+    public Specification<E> dynamicFilter(Map<String, Object> attributes) {
         return (root, query, criteriaBuilder) -> {
             Predicate predicates = criteriaBuilder.conjunction();
 
-            for (Map.Entry<String, Object> entry : attributtes.entrySet()) {
+            for (Map.Entry<String, Object> entry : attributes.entrySet()) {
                 String key = entry.getKey();
                 Object value = entry.getValue();
 
@@ -93,26 +74,55 @@ public class GenericSpecification<E extends BaseEntity> {
 
     public <T extends Comparable<? super T>> Specification<E> filterBetween(T lower, T higher, String field) {
         return (root, query, criteriaBuilder) -> {
-            T finalLower = lower;
-            T finalHigher = higher;
-
-            if (finalLower != null && finalHigher != null && finalHigher.compareTo(finalLower) < 0) {
-                T temp = finalLower;
-                finalLower = finalHigher;
-                finalHigher = temp;
+            if (lower == null && higher == null) {
+                throw new NullPointerException("Null Pointer inputs on filter between.");
             }
 
-            if (finalLower != null && finalHigher != null) {
-                return criteriaBuilder.between(root.get(field), finalLower, finalHigher);
-            } else if (finalLower != null) {
-                return criteriaBuilder.greaterThanOrEqualTo(root.get(field), finalLower);
-            } else if (finalHigher != null) {
-                return criteriaBuilder.lessThanOrEqualTo(root.get(field), finalHigher);
+            if (higher.compareTo(lower) < 0) {
+                throw new IllegalArgumentException("Invalid inputs on filter between.");
             }
 
-            return criteriaBuilder.conjunction();
+            return criteriaBuilder.between(root.get(field), lower, higher);
         };
     }
 
+    public <T extends Comparable<? super T>> Specification<E> filterLowerThan(T value, String field) {
+        return (root, query, criteriaBuilder) -> {
+            if (value == null) {
+                throw new IllegalArgumentException("Invalid input on filter lower than.");
+            }
+
+            return criteriaBuilder.lessThan(root.get(field), value);
+        };
+    }
+
+    public <T extends Comparable<? super T>> Specification<E> filterGreaterThan(T value, String field) {
+        return (root, query, criteriaBuilder) -> {
+            if (value == null) {
+                throw new IllegalArgumentException("Invalid input on filter lower than.");
+            }
+
+            return criteriaBuilder.greaterThan(root.get(field), value);
+        };
+    }
+
+    public Specification<E> dynamicFilterLike(Map<String, Object> attributes) {
+        return (root, query, criteriaBuilder) -> {
+            Predicate predicates = criteriaBuilder.conjunction();
+
+            for (Map.Entry<String, Object> entry : attributes.entrySet()) {
+                String key = entry.getKey();
+                Object value = entry.getValue();
+
+                if (value != null) {
+                    String likePattern = "%" + value.toString().toLowerCase(Locale.ROOT) + "%";
+                    predicates = criteriaBuilder.and(predicates, criteriaBuilder.like(
+                            criteriaBuilder.lower(root.get(key).as(String.class)), likePattern));
+                }
+            }
+
+            return predicates;
+        };
+    }
 
 }
