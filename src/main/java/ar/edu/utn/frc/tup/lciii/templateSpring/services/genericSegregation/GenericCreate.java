f10fbfa68dfcfea3fe084b9017a305925a69db2c
RamiroRomera <112445180+RamiroRomera25@users.noreply.github.com>
2024-11-15 12:34:05 -0300

    Interface Segregation - Create

---
+++ src/main/java/ar/edu/utn/frc/tup/lciii/templateSpring/services/genericSegregation/GenericCreate.java
@@ -0,0 +1,20 @@
+package ar.edu.utn.frc.tup.lciii.templateSpring.services.genericSegregation;
+
+import ar.edu.utn.frc.tup.lciii.templateSpring.repositories.GenericRepository;
+import ar.edu.utn.frc.tup.lciii.templateSpring.repositories.specs.SpecificationBuilder;
+import org.modelmapper.ModelMapper;
+
+public interface GenericCreate<E, I, M, DTOPOST> {
+    ModelMapper getMapper();
+
+    GenericRepository<E, I> getRepository();
+
+    Class<E> entityClass();
+
+    Class<M> modelClass();
+
+    default M create(DTOPOST dtoPost) {
+        E entityToSave = getMapper().map(dtoPost, entityClass());
+        return getMapper().map(getRepository().save(entityToSave), modelClass());
+    }
+}
