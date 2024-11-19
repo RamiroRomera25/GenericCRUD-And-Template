195f5afdbdadd7234bf26b884eebf79af0796a82
RamiroRomera <164185531+RamiroRomera@users.noreply.github.com>
2024-11-19 16:05:52 -0300

    Segregation Generic -> Delete by ID

--- src/main/java/ar/edu/utn/frc/tup/lciii/templateSpring/services/genericSegregation/GenericSoftDelete.java
+++ src/main/java/ar/edu/utn/frc/tup/lciii/templateSpring/services/genericSegregation/GenericSoftDelete.java
@@ -3,15 +3,12 @@ package ar.edu.utn.frc.tup.lciii.templateSpring.services.genericSegregation;
 import ar.edu.utn.frc.tup.lciii.templateSpring.entities.base.BaseEntity;
 import ar.edu.utn.frc.tup.lciii.templateSpring.repositories.GenericRepository;
 import org.modelmapper.ModelMapper;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.server.ResponseStatusException;
 
-import java.util.Optional;
-
-public interface GenericSoftDelete<E extends BaseEntity, I, M> {
+public interface GenericSoftDelete<E extends BaseEntity, I, M> extends GenericGetById<E, I, M> {
     ModelMapper getMapper();
 
     GenericRepository<E, I> getRepository();
+
     Class<M> modelClass();
 
     default M delete(I id) {
@@ -23,14 +20,8 @@ public interface GenericSoftDelete<E extends BaseEntity, I, M> {
     }
 
     private M changeActiveStatus(I id, boolean isActive) {
-        Optional<E> entityOptional = getRepository().findById(id);
-
-        if (entityOptional.isPresent()) {
-            E entity = entityOptional.get();
-            entity.setIsActive(isActive);
-            return getMapper().map(getRepository().save(entity), modelClass());
-        } else {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Not found any object with id: " + id);
-        }
+        E entity = this.getById(id);
+        entity.setIsActive(isActive);
+        return getMapper().map(getRepository().save(entity), modelClass());
     }
 }
