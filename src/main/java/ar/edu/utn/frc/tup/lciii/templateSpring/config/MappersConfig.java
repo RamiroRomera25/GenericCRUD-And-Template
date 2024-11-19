3c88b3876708e3a85d99d3793419102a39f6b59d
RamiroRomera <164185531+RamiroRomera@users.noreply.github.com>
2024-11-19 14:45:39 -0300

    Strict Mapper

--- src/main/java/ar/edu/utn/frc/tup/lciii/templateSpring/config/MappersConfig.java
+++ src/main/java/ar/edu/utn/frc/tup/lciii/templateSpring/config/MappersConfig.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.modelmapper.Conditions;
 import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -20,6 +21,18 @@ public class MappersConfig {
         ModelMapper mapper = new ModelMapper();
         mapper.getConfiguration().setPropertyCondition(Conditions.isNotNull());
         return mapper;
+    }
+
+    /**
+     * The ModelMapper bean to merge strict objects.
+     * @return the ModelMapper to use in updates.
+     */
+    @Bean("strictMapper")
+    public ModelMapper strictMapper() {
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration()
+                .setMatchingStrategy(MatchingStrategies.STRICT);
+        return modelMapper;
     }
 
     @Bean
