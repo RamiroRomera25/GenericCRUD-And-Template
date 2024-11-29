98c5a4ad7d751bfb9e6b2e6edcb8852a649fc23a
RamiroRomera <112445180+RamiroRomera25@users.noreply.github.com>
2024-11-29 03:18:52 -0300

    Optimize Imports

--- src/main/java/rami/generic/controllers/genericSegregation/basicCRUD/ControllerGetByIdList.java
+++ src/main/java/rami/generic/controllers/genericSegregation/basicCRUD/ControllerGetByIdList.java
@@ -1,18 +1,14 @@
 package rami.generic.controllers.genericSegregation.basicCRUD;
 
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import rami.generic.dtos.common.ErrorApi;
-import rami.generic.services.genericSegregation.basicCRUD.GenericGetById;
 import rami.generic.services.genericSegregation.basicCRUD.GenericGetByIdList;
 
 import java.util.List;
