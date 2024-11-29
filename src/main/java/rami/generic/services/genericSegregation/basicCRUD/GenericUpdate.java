f47d933efb3094fe4de7168087364dafb1cacc54
RamiroRomera <112445180+RamiroRomera25@users.noreply.github.com>
2024-11-29 03:16:28 -0300

    Delete comment

--- src/main/java/rami/generic/services/genericSegregation/basicCRUD/GenericUpdate.java
+++ src/main/java/rami/generic/services/genericSegregation/basicCRUD/GenericUpdate.java
@@ -11,18 +11,6 @@ public interface GenericUpdate<E, I, M, DTOPUT> extends GenericGetById<E, I, M> 
 
     Class<M> modelClass();
 
-//    @Deprecated
-//    default M update(DTOPUT dtoPut, I id) {
-//        E entity = this.getModelById(id);
-//        if (optionalEntity.isPresent()) {
-//            E entity = optionalEntity.get();
-//            getMapper().map(dtoPut, entity);
-//            return getMapper().map(getRepository().save(entity), modelClass());
-//        } else {
-//            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Not found any object with id: " + id);
-//        }
-//    }
-
     default M update(DTOPUT dtoPut, I id) {
         E entity = this.getById(id);
         getMapper().map(dtoPut, entity);
