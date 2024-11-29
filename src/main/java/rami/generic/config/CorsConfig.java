8b8eef3cd0ff80f5ad44c758dedad73a0a6f9822
RamiroRomera <112445180+RamiroRomera25@users.noreply.github.com>
2024-11-29 03:31:08 -0300

    Fix

--- src/main/java/rami/generic/config/CorsConfig.java
+++ src/main/java/rami/generic/config/CorsConfig.java
@@ -4,17 +4,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-/**
- * Configuración de CORS  para Spring Boot.
- */
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
-    /**
-     * Configura las reglas CORS para la aplicación.
-     *
-     * @param registry El registro donde se configuran los mapeos CORS.
-     */
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
